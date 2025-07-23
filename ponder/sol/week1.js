2fae97d2e1030597953df7766ece74a0c3cd5b6b
andejuli <techtrek@hotmail.com>
2025-07-23 10:05:16 -0600

    week5prove

--- ponder/sol/week1.js
+++ ponder/sol/week1.js
@@ -5,4 +5,6 @@ if (true) {
     console.log(student); //works just fine, it's being accessed with in the block
 }
 console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
+//console.log(student); //does not work, can't access a block variable outside the block
+
+
