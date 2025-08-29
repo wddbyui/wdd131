1b51a860cd71f132a42f44c08c1744f1ee6e9e8e
andejuli <techtrek@hotmail.com>
2025-08-29 11:27:05 -0600

    objects

--- ponder/sol/week8.js
+++ ponder/sol/week8.js
@@ -1,6 +1,7 @@
 const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
+    logo: "images/js-logo.png",
     sections: [
     { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
     { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
@@ -16,6 +17,14 @@ const aCourse = {
         }
       }
   };
+
+document.querySelector('img').setAttribute('src', aCourse.logo);
+document.querySelector('img').setAttribute('alt', aCourse.name);
+document.querySelector('img').style.width = '100px';
+
+document.querySelector('#courseName').textContent = aCourse.name;
+document.querySelector('#courseCode').textContent = aCourse.code;
+
 
 function sectionTemplate(section) {
     return `<tr>
